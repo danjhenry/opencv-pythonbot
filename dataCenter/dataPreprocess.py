41198ef3e21ef6b817ec70306ca8751e05c58bfc
Daniel Henry <email378637@gmail.com>
2017-07-21 09:55:34 -0700

    Update dataPreprocess.py

--- dataCenter/dataPreprocess.py
+++ dataCenter/dataPreprocess.py
@@ -11,7 +11,7 @@ def load_obj(name):
 def main():
     total = {'skillBoat' : 0, 'superCase' : 0, 'superBoat' : 0, 'skill' : 0, 'legendary' : 0,  'vouchers' : 0, 'draws' : 0, 'super' : 0}
     dctItems = ('skillBoat', 'superCase', 'superBoat', 'skill', 'legendary', 'vouchers', 'draws', 'super')
-    fileName = 'chrome-'
+    fileName = 'firefox-bot'
     fileNames = []
     for x in range(1, 9):
         fileNames.append('C:/Users/email/Desktop/wheel/' + fileName + str(x) + '/statistics/wheel' + str(x))
